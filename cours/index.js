86d42f8e2ad882bebd55fd817e8c6bc11db0504f
ixyhoo <ixyhoo@outlook.fr>
2023-06-15 14:19:57 +0200

    object

--- cours/index.js
+++ cours/index.js
@@ -7,6 +7,15 @@ console.log(obj.pseudo);
 
 let array = [3, 5, 8]
 
-console.log(document.body);
+/*console.log(document.body);
 console.log(typeof document.body);
-console.log(typeof array);
+console.log(typeof array);*/
+
+obj.age = 24;
+console.log(obj);
+
+obj.pseudo = "fly";
+
+delete obj.ville;
+console.log(obj);
+
