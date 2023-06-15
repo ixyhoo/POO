fd693ab7edc70bb230d6b4a5b61d47fdc16e7552
ixyhoo <ixyhoo@outlook.fr>
2023-06-15 14:15:27 +0200

    object

---
+++ cours/index.js
@@ -0,0 +1,12 @@
+const obj = {
+    pseudo: "jha",
+    ville: "casablanca",
+}
+console.log(typeof obj);
+console.log(obj.pseudo);
+
+let array = [3, 5, 8]
+
+console.log(document.body);
+console.log(typeof document.body);
+console.log(typeof array);
