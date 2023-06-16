82a89f25dcf3591fe8ca6e9b55bc2bf9eef03992
ixyhoo <ixyhoo@outlook.fr>
2023-06-16 06:29:22 +0200

    native methode

--- cours/index.js
+++ cours/index.js
@@ -1,9 +1,16 @@
 const obj = {
     pseudo: "jha",
     ville: "casablanca",
+    admin: false,
+direBonjour: function(){
+    console.log("bonjour je suis " + this.pseudo + " j'habite à " + this.ville);
 }
-console.log(typeof obj);
-console.log(obj.pseudo);
+    /*direBonjour(){
+        console.log("bonjour je suis " + this.pseudo + " j'habite à " + this.ville);
+    }*/
+}
+//console.log(typeof obj);
+//console.log(obj.pseudo);
 
 let array = [3, 5, 8]
 
@@ -12,10 +19,18 @@ console.log(typeof document.body);
 console.log(typeof array);*/
 
 obj.age = 24;
-console.log(obj);
+//console.log(obj);
 
 obj.pseudo = "fly";
 
-delete obj.ville;
-console.log(obj);
+//delete obj.ville;
+//console.log(obj);
 
+//checher si propriété existe
+//console.log("pseudo" in obj);
+//console.log("ville" in obj);
+//parcourir un objet
+for(let key in obj){
+    //console.log(key + ":" + obj[key]);
+}
+console.log(obj.direBonjour());
